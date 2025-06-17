8e10851085679c870069773a8a78c7e83802ccd8
otorquati <torquati.osvaldo@gmail.com>
2025-06-17 14:25:38 -0300

    feat: changed the landing page to  Dave Gray example

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,29 +1,29 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import Listening from "../../assets/LiveMixDJ-removebg.png"
-import './style.css';
+import Listening from "../../assets/LiveMixDJ-removebg.png";
+import "./style.css";
 import Container from "../../components/Container";
+import Landing from "../Landing";
 function Home() {
   return (
     <>
       <Header />
-      <Container>
-        <section className="home">
+  
+        <div className="home">
           <div>
             <p>
               Bem-vindo ao seu caderninho <span>Livemix</span>
               <br />
               O local certo para você marcar suas músicas <br />
               preferidas enquanto ouve o <span>LiveMix</span>
-            </p> 
+            </p>
           </div>
           <figure>
             <img src={Listening} alt="" />
           </figure>
-        </section>
-      </Container>
+        </div>
       <Footer />
     </>
   );
 }
-export default Home
+export default Home;
--- frontend/src/pages/Landing/index.jsx
+++ frontend/src/pages/Landing/index.jsx
@@ -1,106 +1,278 @@
 import "./style.css";
 import { GoogleLogin } from "@react-oauth/google";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { jwtDecode } from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
-import GoogleIcon from "../../assets/googleicon-48.png";
-// import googleIcon from "../../assets/googleicon-48.png"
-function Landing() {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [profilePic, setProfilePic] = useState();
+
+// User regex to control the constraints of each field
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,32}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX = /^[a-z0-9.]{1,64}@[a-z0-9.]{1,64}$/;
+
+import React from "react";
+import Container from '../../components/Container/index';
+
+const Landing = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  // Campos da página Landing
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [profilePic, setProfilePic] = useState("");
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    const result = EMAIL_REGEX.test(email);
+    setValidEmail(result);
+  }, [email]);
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd, matchPwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefaul();
+    // if button enabled with JS hack
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || !v2) {
+      setErrMsg("Dados Inválidos");
+      return;
+    }
+    setSuccess(true);
+  };
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigates = useNavigate();
+
   return (
-    <div>
-      <form action="" className="login">
-        <h2>Login Page</h2>
-        <div className="campos">
-          <input
-            type="text"
-            name="user"
-            placeholder="Nome"
-            className="inputs"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="seu melhor email"
-            className="inputs"
-          />
-          <input
-            type="password"
-            name="senha"
-            placeholder="sua senha"
-            className="inputs"
-          />
-          <div className="lnk">
-          <button>Cadastrar</button>
-          <button>Esqueci minha senha</button>
-          </div>
-          <button className="btn">Logar com e-mail</button>
+    <>
+    {success ? (
+      <section>
+        <h1>Sucesso!</h1>
+        <p>
+          <a href="/home">Logado</a>
+        </p>
+        <Home />
+      </section>
+    ):(
+      <section>
+        <p
+          ref={errRef}
+          className={errMsg ? "errmsg" : "offscreen"}
+          aria-live="assertive"
+        >
+          {errMsg}
+        </p>
+        <h1>Login / Cadastro</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">
+              Usuário:
+              <span className={validName ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validName || !user ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              required
+              aria-invalid={validName ? "false" : "true"}
+              aria-describedby="uidnote"
+              onFocus={() => setUserFocus(true)}
+              onBlur={() => setUserFocus(false)}
+            />
+            <p
+              id="uidnote"
+              className={
+                userFocus && user && !validName ? "instructions" : "offscreen"
+              }
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              4 a 24 caracteres,
+              <br />
+              Deve começar com uma letra. <br />
+              Letras, números, sublinhados e hifens são permitidos.
+            </p>
+            <label htmlFor="email">
+              Email:
+              <span className={validEmail ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validEmail || !email ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              aria-invalid={validEmail ? "false" : "true"}
+              aria-describedby="emailaddr"
+              onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
+            />
+            <p
+              id="emailaddr"
+              className={
+                emailFocus && email && !validEmail
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              digite um e-mail valido
+              <br />
+              Formato: identificação@provedor
+            </p>
+
+            <label htmlFor="password">
+              Senha:
+              <span className={validPwd ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              required
+              aria-invalid={validPwd ? "false" : "true"}
+              aria-describedby="pwdnote"
+              onFocus={() => setPwdFocus(true)}
+              onBlur={() => setPwdFocus(false)}
+            />
+            <p
+              id="pwdnote"
+              className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              8 a 24 caracteres. <br />
+              Deve incluir maiúculas, minúsculas, números e caracteres
+              especiais. <br />
+              Caracteres especiais permitidos:
+              <span aria-label="exclamation mark">!</span>
+              <span aria-label="at-symbol">@</span>
+              <span aria-label="hashtag">#</span>
+              <span aria-label="dollar sign">$</span>
+              <span aria-label="percent">%</span>
+            </p>
+
+            <label htmlFor="password">
+              Confirme sua Senha:
+              <span className={validMatch ? "valid" : "hide"}>
+                <FontAwesomeIcon icon={faCheck} />
+              </span>
+              <span className={validMatch || !pwd ? "hide" : "invalid"}>
+                <FontAwesomeIcon icon={faTimes} />
+              </span>
+            </label>
+            <input
+              type="password"
+              id="confirm_pwd"
+              onChange={(e) => setMatchPwd(e.target.value)}
+              required
+              aria-invalid={validMatch ? "false" : "true"}
+              aria-describedby="confirmnote"
+              onFocus={() => setMatchFocus(true)}
+              onBlur={() => setMatchFocus(false)}
+            />
+            <p
+              id="confirmnote"
+              className={
+                matchFocus && !validMatch ? "instructions" : "offscreen"
+              }
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              Deve ser igual a digitada no campo Senha
+            </p>
+
+            <button
+              disabled={!validName || !validPwd || !validMatch ? true : false}
+            >
+              Cadastrar
+            </button>
+        </form>
+        <div className="google">
           <GoogleLogin
             onSuccess={(credentialResponse) => {
               const decode = jwtDecode(credentialResponse.credential);
-              setName(decode.name);
+              setUser(decode.name);
               setEmail(decode.email);
+              setSuccess(true);
               setProfilePic(decode.picture);
               setIsLoggedIn(true);
               navigates("/");
             }}
             onError={() => console.log("Falha no Login")}
-          />
+            />
         </div>
-      </form>
 
-      {isLoggedIn ? (
-        <div className="login">
-          <img src={profilePic} className="img" alt="Profile" />
-          <p>Nome: {name}</p>
-          <p>Email: {email}</p>
-        </div>
-      ) : (
-        ""
-      )}
-    </div>
-  );
-}
-{
-  /*
-    </div>
-      <Container>
-        <form action="" className=login}>
-          <h2>Login Page</h2>
-          <div className={style.campos}>
-            <input
-              type="text"
-              name="user"
-              placeholder="Nome"
-              className={style.inputs}
-            />
-            <input
-              type="email"
-              name="user"
-              placeholder="seu melhor email"
-              className={style.inputs}
-            />
-            <button className={style.btn}>
-              Logar com e-mail
-            </button>
-            <button className={style.btn}>
-              <img
-                src={GoogleIcon}
-                className={style.icon}
-                alt="Logar com google"
-              />
-              Google
-            </button>
+        {isLoggedIn ? (
+          <div className="login">
+            <img src={profilePic} className="img" alt="Profile" />
+            <p>Nome: {user}</p>
+            <p>Email: {email}</p>
           </div>
-        </form>
-      </Container>
-      </>
-    );
-  }
-    */
-}
+        ) : (
+          ""
+        )}
+      </section>)}
+    </>
+  );
+};
+
 export default Landing;
